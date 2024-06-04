3b1c2cd1729c687cd629aafa8e0b7130e2459d5c
Luiz Lima <lima.luizgo@gmail.com>
2024-06-04 19:28:07 -0300

    Initial Commit

---
+++ fiap-sighting-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fiap-sighting-ios
+//
+//  Created by Luiz Lima on 04/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fiap-sighting-ios/fiap_sighting_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  fiap_sighting_iosApp.swift
+//  fiap-sighting-ios
+//
+//  Created by Luiz Lima on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct fiap_sighting_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
